1bd7628052eb72f91a86d23a4ade233062a7d6bd
kod-python <kelvinlik0245@gmail.com>
2023-12-28 00:06:42 +0000

    big add2

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+
+// funstion for time and date
+function myTimer() {
+  var d = new Date();
+  document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+}
+var myVar = setInterval(myTimer, 1000);
+
+
+
+// end of funsction on date and time
+
+
+
+
+
+
+
+
 // better swipper 
 
 var swiper = new Swiper(".review-slider", {
@@ -20,3 +39,4 @@ var swiper = new Swiper(".review-slider", {
     },
 });
 // better swipper ends here
+
