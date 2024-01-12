07e132481fce8d1d1adc30ddd3385b41677fd8db
kod-python <kelvinlik0245@gmail.com>
2024-01-12 13:51:23 +0000

    reload build

--- script.js
+++ script.js
@@ -1,42 +1,5 @@
-
-// funstion for time and date
-function myTimer() {
-  var d = new Date();
-  document.getElementById("demo").innerHTML = d.toLocaleTimeString();
-}
-var myVar = setInterval(myTimer, 1000);
-
-
-
-// end of funsction on date and time
-
-
-
-
-
-
-
-
-// better swipper 
-
-var swiper = new Swiper(".review-slider", {
-    spaceBetween: 20,
-    loop:true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        640: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-        },
-    },
+$(document).ready(function(){
+$(".hamburger").click(function(){
+  $('.aside').fadeToggle(100)
 });
-// better swipper ends here
-
+});
